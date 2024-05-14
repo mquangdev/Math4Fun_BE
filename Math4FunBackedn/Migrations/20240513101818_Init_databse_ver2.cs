75cbd55ee8b647ebce74d18fab43f6cc1f652945
mquangdev <minhquang29102002@gmail.com>
2024-05-14 08:45:20 +0700

    update at company

---
+++ Math4FunBackedn/Migrations/20240513101818_Init_databse_ver2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Math4FunBackedn.Migrations
+{
+    public partial class Init_databse_ver2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
